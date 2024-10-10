f90cd944232ba31e33e82bd199860bfaae7e1b42
zaidansari-RCOE <zaidansari@eng.rizvi.edu.in>
2024-10-10 23:55:37 +0530

    Create Expt1Task1.c
    first experiment for the semester

---
+++ Expt1Task1.c
@@ -0,0 +1,36 @@
+/*  Name    : Mohd Zaid Mashooque Ansari
+    Division: C3
+    UIN     : 241P057
+    Title   : WAP to print your name, age,class,division and UIN on the screen.Use escape sequences to properly format the output. */
+
+#include<stdio.h>
+    int main()
+    {
+        printf("\t\t***MY INTRODUCTION***\n\n\n\n");
+        printf("\tName  \t\t: \tMohd Zaid Mashooque Ansari\n\n");
+        printf("\tAge  \t\t: \t19\n\n");
+        printf("\tClass  \t\t: \tFirst Year Engineering\n\n");
+        printf("\tDivision \t: \tC3\n\n");
+        printf("\tUIN  \t\t: \t241P057\n\n");
+
+        return 0;
+    }
+
+    /*Output:
+                    ***MY INTRODUCTION***
+
+
+
+        Name            :       Mohd Zaid Mashooque Ansari
+
+        Age             :       19
+
+        Class           :       First Year Engineering
+
+        Division        :       C3
+
+        UIN             :       241P057
+
+
+Process returned 0 (0x0)   execution time : 0.064 s
+Press any key to continue.*/
